5fc76fa862b2d20e08e9f1a8f10a4ec61925a090
Chris1278 <31364765+chris1278@users.noreply.github.com>
2022-05-06 23:29:56 +0200

    Update ext.php

--- chris1278/statblockbridge/ext.php
+++ chris1278/statblockbridge/ext.php
@@ -35,18 +35,5 @@ class ext extends \phpbb\extension\base
 		{
 			trigger_error(sprintf($language->lang('EXTENSION_NOT_ENABLEABLE') . '<br />' . $language->lang('SBBRIDGE_ERROR_PHPBB_MISTMATCH'), $phpbb_min_ver, $phpbb_below_ver) . $this->get_adm_back_link(), E_USER_WARNING);
 		}
-
-		/**
-		* Now if the extension is enabled, first.
-		*/
-		$ext_manager = $this->container->get('ext.manager');
-
-		if (!$ext_manager->is_enabled('kirk/statblock'))
-		{
-
-				trigger_error(sprintf($language->lang('EXTENSION_NOT_ENABLEABLE') . '<br />' . $language->lang('SBBRIDGE_ERROR_STATBLOCK_NOT_INSTALLED')). adm_back_link(append_sid('index.' . $this->container->getParameter('core.php_ext'), 'i=acp_extensions&amp;mode=main')), E_USER_WARNING);
-		}
-
-		return $is_enableable;
 	}
 }
